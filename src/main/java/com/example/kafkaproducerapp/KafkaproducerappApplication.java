333ef9738c070fc8103e13969c8b76e9fb15439e
Pawan Kumar <pawan41281@gmail.com>
2025-03-18 14:07:09 +0530

    initial commit

---
+++ src/main/java/com/example/kafkaproducerapp/KafkaproducerappApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaproducerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaproducerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaproducerappApplication.class, args);
+	}
+
+}
